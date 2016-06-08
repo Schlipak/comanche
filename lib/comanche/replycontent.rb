91e111623babe999c5d0b86c3a38b89a57f723e1
de-mat_b <de-mat_b@epitech.eu>
2016-06-08 23:22:18 +0200

    Added svg content-type support

--- lib/comanche/replycontent.rb
+++ lib/comanche/replycontent.rb
@@ -14,6 +14,7 @@ module Comanche
       'jpg' => 'image/jpeg',
       'jpeg' => 'image/jpeg',
       'gif' => 'image/gif',
+      'svg' => 'image/svg+xml'
       'ico' => 'image/x-icon',
       'css' => 'text/css',
       'js' => 'text/javascript',
